4c2ed7ad4a74141de7a1aa6ea27cf3eaace9eda9
Carolina Filipe <anilorac.filipe@gmail.com>
2022-10-17 18:11:54 +0200

    initial commit

---
+++ sketch.js
@@ -0,0 +1,21 @@
+let x, y, r, g, b;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(0);
+}
+
+function draw() {
+  r = random(255);
+  g = random(255);
+  b = random(255);
+  x = random(width);
+  y = random(height);
+  fill(r, g, b, 150);
+  noStroke();
+  circle(x, y, 20);
+}
+
+function mouseClicked() {
+  background(0);
+}
